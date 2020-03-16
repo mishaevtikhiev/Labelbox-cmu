195be9cd774db061c88b00a4dee2b6a3f93967b6
n-buga <bugakova_nadya@mail.ru>
2020-03-16 15:19:56 +0300

    Change lexer to python lexer & replace extension with img

--- highlighter.py
+++ highlighter.py
@@ -1,11 +1,11 @@
 import os
 from pygments import highlight
-from pygments.lexers import CppLexer
+from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 
-dataset = '/home/egor/Work/Labelbox/compressed_for_labeling/labeling_sampled_submissions/sampled_problems_1000_9999'
+dataset = 'test_dataset'
 for filename in os.listdir(dataset):
     code = open(f'{dataset}/{filename}', 'r').read()
-    text = highlight(code, CppLexer(), HtmlFormatter(full=True, style='colorful'))
-    open(f'highlighted_1000_9999/{filename.replace("cpp", "txt")}', 'w').write(text)
+    text = highlight(code, PythonLexer(), HtmlFormatter(full=True, style='colorful'))
+    open(f'highlited_dataset/{filename.replace("py", "jpg")}', 'w').write(text)
 
