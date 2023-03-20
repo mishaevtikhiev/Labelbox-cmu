bbb17520ee0bfddeed0270ac683936696e0a8852
Misha Evtikhiev <mikhail.evtikhiev@jetbrains.com>
2023-03-20 17:55:49 +0200

    add highlighted code datasets

--- highlighter.py
+++ highlighter.py
@@ -1,11 +1,26 @@
 import os
 from pygments import highlight
 from pygments.lexers import PythonLexer
+from pygments.lexers import JavascriptLexer
+from pygments.lexers import CppLexer
+from pygments.lexers import JavaLexer
 from pygments.formatters import HtmlFormatter
+import json
+lexers_dict = {"js": JavascriptLexer(), "cpp": CppLexer(), "java": JavaLexer()}
 
-dataset = 'test_dataset'
+dataset = 'cmu_datasets'
 for filename in os.listdir(dataset):
-    code = open(f'{dataset}/{filename}', 'r').read()
-    text = highlight(code, PythonLexer(), HtmlFormatter(full=True, style='colorful'))
-    open(f'highlited_dataset/{filename.replace("py", "jpg")}', 'w').write(text)
+    lang = filename[:-5].split('_')[-1]
+    lexer = lexers_dict[lang]
+    with open(f'{dataset}/{filename}') as f:
+        code_dataset = json.load(f)
+    for task in code_dataset:
+        task_id = task["task_id"]
+        snippets = task["sample_from"].keys()
+        for snippet_number in snippets:
+            code = task[snippet_number]
+            snippet_number_to_int = int(snippet_number)
+            text = highlight(code, lexer, HtmlFormatter(full=True, style='colorful'))
+            open(f'highlighted_dataset/{lang}/{task_id}_{snippet_number_to_int}.html', 'w').write(text)
+
 
